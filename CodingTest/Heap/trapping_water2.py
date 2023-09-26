15449ebcbd7e807ac696f13effb767ad337d14a0
chycs7747 <chycs98@naver.com>
2023-09-27 02:54:47 +0900

    url added hotel, trapping water2

--- CodingTest/Heap/trapping_water2.py
+++ CodingTest/Heap/trapping_water2.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/trapping-rain-water-ii/description/
+
 import heapq
 
 class Solution:
--- CodingTest/Queue/hotel.py
+++ CodingTest/Queue/hotel.py
@@ -1,3 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/155651
+
 import heapq
 
 def solution(book_time):
