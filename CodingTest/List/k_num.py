86db2db1f74f15bfbb756f78811f85427638a679
chycs7747 <chycs98@naver.com>
2023-12-31 19:36:59 +0900

    sorted review

---
+++ CodingTest/List/k_num.py
@@ -0,0 +1,7 @@
+#코딩테스트 연습 > 정렬 > k번째수
+#url - https://school.programmers.co.kr/learn/courses/30/lessons/42748
+def solution(array, commands):
+    answer = []
+    for i,j,k in commands:
+        answer.append(sorted(array[i-1:j])[k-1])
+    return answer
